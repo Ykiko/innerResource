642dd269dba51b4d2c4d48619dcefa3ae485880d
grigory-yki <z.d.0.r.0.v.g.k@gmail.com>
2018-11-08 16:17:05 +0400

    Initial commit

---
+++ src/main/java/com/waveaccess/innerResource/InnerResourceApplication.java
@@ -0,0 +1,12 @@
+package com.waveaccess.innerResource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerResourceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/waveaccess/innerResource/InnerResourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.waveaccess.innerResource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InnerResourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
